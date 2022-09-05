99c908a8aae296e0608286da6a8f152d6b2b537f
shay <shaymoshe2008@gmail.com>
2022-09-05 17:23:41 +0300

    Transporter setState()

---
+++ src/main/java/frc/trigon/robot/subsystems/transporter/Transporter.java
@@ -0,0 +1,28 @@
+package frc.trigon.robot.subsystems.transporter;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Transporter extends SubsystemBase {
+
+
+    private final static Transporter INSTANCE = new Transporter();
+    private final WPI_TalonSRX motor;
+    public static Transporter getInstance() {
+        return INSTANCE;
+    }
+    private Transporter() {
+        motor = TransporterConstants.MOTOR;
+    }
+    public void setState (TransporterConstants.TransporterState state){
+        motor.set(ControlMode.PercentOutput,state.voltage/TransporterConstants.VOLTAGE_COMPENSATION);
+
+
+    }
+
+
+
+
+}
+
