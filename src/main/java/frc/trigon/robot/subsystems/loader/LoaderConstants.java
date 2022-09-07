5a5adfc0f7a8a242c34a276055612190a0a8ef90
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-09-07 19:30:50 +0300

    LoaderConstants

---
+++ src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
@@ -0,0 +1,21 @@
+package frc.trigon.robot.subsystems.loader;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class LoaderConstants {
+    private static final int MOTOR_ID = 0;
+    protected static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(MOTOR_ID);
+    protected static final double LOAD_VOLTAGE = 0;
+    protected static final double EJECT_VOLTAGE = 0;
+    protected static final double VOLTAGE_COMPENSATION = 10.5;
+
+    public static enum LoaderState
+    {
+        LOAD(LOAD_VOLTAGE),
+        OFF(0),
+        EJECT(EJECT_VOLTAGE);
+
+        final double voltage;
+        LoaderState(double voltage){this.voltage=voltage;}
+    }
+}
