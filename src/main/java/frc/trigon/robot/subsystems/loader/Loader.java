327b5aec4ca932602a5760a1a13d0d39aebdf280
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-09-07 19:30:34 +0300

    Loader

---
+++ src/main/java/frc/trigon/robot/subsystems/loader/Loader.java
@@ -0,0 +1,27 @@
+package frc.trigon.robot.subsystems.loader;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Loader extends SubsystemBase {
+    private WPI_TalonSRX motor;
+
+    private final static Loader INSTANCE = new Loader();
+
+
+    public static Loader getInstance() {
+        return INSTANCE;
+    }
+
+    private Loader() {
+        motor = LoaderConstants.MOTOR;
+    }
+
+    public void setState(LoaderConstants.LoaderState state) {
+        motor.set(ControlMode.PercentOutput,state.voltage/LoaderConstants.VOLTAGE_COMPENSATION);
+    }
+
+
+}
+
