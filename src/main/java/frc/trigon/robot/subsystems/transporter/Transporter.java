aa18e6a0199dcbe5773644a4e23e16a68091205a
shay <shaymoshe2008@gmail.com>
2022-09-08 17:14:28 +0300

    Reformatting & transporter voltage

--- src/main/java/frc/trigon/robot/subsystems/transporter/Transporter.java
+++ src/main/java/frc/trigon/robot/subsystems/transporter/Transporter.java
@@ -5,27 +5,24 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Transporter extends SubsystemBase {
-
-
     private TransporterConstants.TransporterState currentState;
     private final static Transporter INSTANCE = new Transporter();
     private final WPI_TalonSRX motor;
+
     public static Transporter getInstance() {
         return INSTANCE;
     }
+
     private Transporter() {
         motor = TransporterConstants.MOTOR;
     }
-    public void setState (TransporterConstants.TransporterState state){
-        motor.set(ControlMode.PercentOutput,state.voltage/TransporterConstants.VOLTAGE_COMPENSATION);
+
+    public void setState(TransporterConstants.TransporterState state) {
+        motor.set(ControlMode.PercentOutput, state.voltage / TransporterConstants.VOLTAGE_COMPENSATION);
         currentState = state;
     }
-    public TransporterConstants.TransporterState getState(){
+    public TransporterConstants.TransporterState getState() {
         return currentState;
     }
-
-
-
-
 }
 
