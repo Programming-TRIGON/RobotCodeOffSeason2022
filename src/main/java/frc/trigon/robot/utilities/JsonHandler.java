681c1468546c232bac5f7a9c91c021826212b91c
thesnipe12 <yishi178@gmail.com>
2022-09-11 17:48:39 +0300

    JsonHandler

---
+++ src/main/java/frc/trigon/robot/utilities/JsonHandler.java
@@ -0,0 +1,52 @@
+package frc.trigon.robot.utilities;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.wpi.first.wpilibj.Filesystem;
+
+import java.io.*;
+
+public class JsonHandler {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static final String path = Filesystem.getDeployDirectory().getPath() + "\\";
+
+    public static void saveToJsonFile(Object object, String name) {
+        saveFile(JsonHandler.path + name + ".tmp", object);
+        renameFile(JsonHandler.path + name, JsonHandler.path + name + ".bak");
+        renameFile(JsonHandler.path + name + ".tmp", JsonHandler.path + name);
+        deleteFile(JsonHandler.path + name + ".bak");
+    }
+
+    public static <T> T getObjectFromJson(String fileName, Class<T> type) {
+        try(Reader reader = new FileReader(JsonHandler.path + fileName)) {
+            return gson.fromJson(reader, type);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void renameFile(String oldPath, String newPath) {
+        File file = new File(oldPath);
+        if(!file.renameTo(new File(newPath))) {
+            System.out.println("Failed to rename file.");
+        }
+    }
+
+    private static void deleteFile(String path) {
+        File file = new File(path);
+        if(!file.delete()) {
+            System.out.println("Failed to delete file.");
+        }
+    }
+
+    private static void saveFile(String path, Object text) {
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            gson.toJson(text, fileWriter);
+            fileWriter.close();
+        } catch(IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
