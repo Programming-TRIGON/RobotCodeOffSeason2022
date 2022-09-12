815396485d57301cd981df6215538dce8993d09d
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-09-12 17:34:57 +0300

    changed according to the comments

--- src/main/java/frc/trigon/robot/subsystems/loader/Loader.java
+++ src/main/java/frc/trigon/robot/subsystems/loader/Loader.java
@@ -1,6 +1,5 @@
 package frc.trigon.robot.subsystems.loader;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.trigon.robot.subsystems.loader.LoaderConstants.LoaderState;
@@ -21,7 +20,7 @@ public class Loader extends SubsystemBase {
     }
 
     public void setState(LoaderState state) {
-        motor.set(ControlMode.PercentOutput, state.voltage / LoaderConstants.VOLTAGE_COMPENSATION);
+        motor.set(state.power / LoaderConstants.VOLTAGE_COMPENSATION);
         currentState = state;
     }
 
--- src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
@@ -4,21 +4,21 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class LoaderConstants {
     private static final int MOTOR_ID = 0;
+    static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(MOTOR_ID);
     static final double
             LOAD_VOLTAGE = 6,
             EJECT_VOLTAGE = -6;
     static final double VOLTAGE_COMPENSATION = 10.5;
-    static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(MOTOR_ID);
 
     public static enum LoaderState {
         LOAD(LOAD_VOLTAGE),
         OFF(0),
         EJECT(EJECT_VOLTAGE);
 
-        final double voltage;
+        final double power;
 
         LoaderState(double voltage) {
-            this.voltage = voltage;
+            this.power = voltage;
         }
     }
 }
