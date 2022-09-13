1a11aceab099e6aa7d7feb485103b57e6f0f4659
shay <shaymoshe2008@gmail.com>
2022-09-13 10:49:48 +0300

    pitcher

---
+++ src/main/java/frc/trigon/robot/subsystem/pitcher/Pitcher.java
@@ -0,0 +1,35 @@
+package frc.trigon.robot.subsystem.pitcher;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pitcher extends SubsystemBase {
+    private final WPI_TalonSRX motor;
+
+    private final static Pitcher INSTANCE = new Pitcher();
+
+    public static Pitcher getInstance() {
+        return INSTANCE;
+    }
+
+    private Pitcher() {
+        motor = PitcherConstants.MOTOR;
+    }
+
+    public void setPosition() {
+
+    }
+
+    public double ticksToDegrees(double ticks) {
+        double rotations = ticks / 4096;
+        double degrees = rotations * 360;
+        return degrees / PitcherConstants.GEAR_RATIO;
+    }
+
+    public double degreesToTicks(double degrees) {
+        double rotations = degrees / 360;
+        double ticks = rotations * 4096;
+        return ticks / PitcherConstants.GEAR_RATIO;
+    }
+}
+
---
+++ src/main/java/frc/trigon/robot/subsystem/pitcher/PitcherConstants.java
@@ -0,0 +1,10 @@
+package frc.trigon.robot.subsystem.pitcher;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class PitcherConstants {
+    static final int MIN_TICKS = 300, MUX_TICKS =4000;
+    static final double GEAR_RATIO = 10;
+    private static final int MOTOR_ID = 0;
+    final static WPI_TalonSRX  MOTOR = new WPI_TalonSRX(MOTOR_ID);
+}
