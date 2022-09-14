fb6eb1e169514a8f9dbeb72878c3ea83403d28ea
thesnipe12 <yishi178@gmail.com>
2022-09-14 14:01:31 +0300

    Changed the documentation, method names, final variable names to caps, made deleteFile() method to throw an exception and renameFile() method to throw one

--- src/main/java/frc/trigon/robot/utilities/JsonHandler.java
+++ src/main/java/frc/trigon/robot/utilities/JsonHandler.java
@@ -7,31 +7,30 @@ import edu.wpi.first.wpilibj.Filesystem;
 import java.io.*;
 
 public class JsonHandler {
-    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-    private static final String path = Filesystem.getDeployDirectory().getPath() + "\\";
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final String PATH = Filesystem.getDeployDirectory().getPath() + "\\";
 
     /**
-     * Parses the given object to a new or existing JSON file,
-     * using a safe way of saving. (creating a temporary file and then
-     * replacing the original file with the temporary one)
+     * Parses the given object to JSON and writing to a JSON file,
+     * using a safe way of writing.
      *
      * @param object the object to save
      * @param name   the name of the file to save to or create
      */
-    public static void saveToJsonFile(Object object, String name) throws IOException {
+    public static void parseToJsonAndWrite(Object object, String name) throws IOException {
         safeWrite(parseObjectToJson(object), name);
     }
 
     /**
-     * Parsing a JSON file to an object.
+     * Parses a JSON file to an object.
      *
-     * @param fileName the name of the file to read from
-     * @param type     the type of the object to parse
-     * @return the parse
+     * @param fileName the name of the file to read
+     * @param type     the class to parse JSON to
+     * @return the class
      */
     public static <T> T parseJsonToObject(String fileName, Class<T> type) {
-        try(Reader reader = new FileReader(JsonHandler.path + fileName)) {
-            return gson.fromJson(reader, type);
+        try(Reader reader = new FileReader(JsonHandler.PATH + fileName)) {
+            return GSON.fromJson(reader, type);
         } catch(IOException e) {
             e.printStackTrace();
         }
@@ -39,28 +38,28 @@ public class JsonHandler {
     }
 
     private static void safeWrite(String text, String name) throws IOException {
-        writeStringToFile(JsonHandler.path + name + ".tmp", text);
-        renameFile(JsonHandler.path + name, JsonHandler.path + name + ".bak");
-        renameFile(JsonHandler.path + name + ".tmp", JsonHandler.path + name);
-        deleteFile(JsonHandler.path + name + ".bak");
+        writeStringToFile(JsonHandler.PATH + name + ".tmp", text);
+        renameFile(JsonHandler.PATH + name, JsonHandler.PATH + name + ".bak");
+        renameFile(JsonHandler.PATH + name + ".tmp", JsonHandler.PATH + name);
+        deleteFile(JsonHandler.PATH + name + ".bak");
     }
 
-    private static void renameFile(String oldPath, String newPath) {
+    private static void renameFile(String oldPath, String newPath) throws IOException{
         File file = new File(oldPath);
         if(!file.renameTo(new File(newPath))) {
-            System.out.println("Failed to rename file.");
+            throw new IOException("Failed to rename file " + oldPath + " to " + newPath);
         }
     }
 
-    private static void deleteFile(String path) {
+    private static void deleteFile(String path) throws IOException{
         File file = new File(path);
         if(!file.delete()) {
-            System.out.println("Failed to delete file.");
+            throw new IOException("Failed to delete file.");
         }
     }
 
     private static String parseObjectToJson(Object object) {
-        return gson.toJson(object);
+        return GSON.toJson(object);
     }
 
     private static void writeStringToFile(String path, String text) throws IOException {
