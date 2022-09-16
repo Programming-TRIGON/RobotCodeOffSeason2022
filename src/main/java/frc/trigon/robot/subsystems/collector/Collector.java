1a6198dab02073113eb0dc53ef4d49b0181ec6c1
Mboesn <yishayamir@gmail.com>
2022-09-16 12:33:03 +0300

    Reformat and added private and delete protected

--- src/main/java/frc/trigon/robot/subsystems/collector/Collector.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/Collector.java
@@ -5,11 +5,10 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Collector extends SubsystemBase {
-    private static final WPI_TalonFX
+    private final static Collector INSTANCE = new Collector();
+    private final WPI_TalonFX
             openingMotor = CollectorConstants.OPENER_MOTOR,
             collectingMotor = CollectorConstants.COLLECTOR_MOTOR;
-
-    private final static Collector INSTANCE = new Collector();
 
     private Collector() {
 
--- src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/collector/CollectorConstants.java
@@ -5,22 +5,22 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class CollectorConstants {
     protected static final double
-            COLLECTING_POWER = 1,
-            OPENING_POWER = 1,
-            EJECTING_POWER = -1,
-            CLOSING_POWER = -1;
-    static final double
+            COLLECTING_POWER = 0.5,
+            OPENING_POWER = 0.5,
+            EJECTING_POWER = -0.5,
+            CLOSING_POWER = -0.5;
+    private static final double
             OPENER_CURRENT_LIMIT_PEAK_CURRENT = 10,
             OPENER_CURRENT_LIMIT_PEAK_DURATION = 0.2,
             OPENER_CURRENT_LIMIT = 0.5;
-    static final double
+    private static final double
             COLLECTOR_CURRENT_LIMIT_PEAK_CURRENT = 20,
             COLLECTOR_CURRENT_LIMIT_PEAK_DURATION = 0.8,
             COLLECTOR_CURRENT_LIMIT = 8;
     private static final int
             OPENER_MOTOR_ID = 0,
             COLLECTOR_MOTOR_ID = 1;
-    protected static final WPI_TalonFX
+    static final WPI_TalonFX
             OPENER_MOTOR = new WPI_TalonFX(OPENER_MOTOR_ID),
             COLLECTOR_MOTOR = new WPI_TalonFX(COLLECTOR_MOTOR_ID);
     private static final boolean
@@ -28,9 +28,9 @@ public class CollectorConstants {
             COLLECTOR_MOTOR_INVERTED = false;
 
     static {
-
         OPENER_MOTOR.setInverted(OPENER_MOTOR_INVERTED);
         COLLECTOR_MOTOR.setInverted(COLLECTOR_MOTOR_INVERTED);
+        
         OPENER_MOTOR.configStatorCurrentLimit(
                 new StatorCurrentLimitConfiguration(
                         true,
@@ -39,7 +39,6 @@ public class CollectorConstants {
                         OPENER_CURRENT_LIMIT_PEAK_DURATION
                 )
         );
-
         COLLECTOR_MOTOR.configStatorCurrentLimit(
                 new StatorCurrentLimitConfiguration(
                         true,
