3840fb7833fd84587f2885ca2f7c7ffb5f9e4afb
carmigigangen <96187873+carmigigangen@users.noreply.github.com>
2022-09-18 17:40:40 +0300

    revert changes to Robot

--- src/main/java/frc/trigon/robot/Robot.java
+++ src/main/java/frc/trigon/robot/Robot.java
@@ -8,7 +8,8 @@ package frc.trigon.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.trigon.robot.utilities.ShootingCalculations;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the methods corresponding to
