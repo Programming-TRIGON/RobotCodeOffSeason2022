a46006c83ba564aa74d6c92281626b7569636ae8
carmigigangen <96187873+carmigigangen@users.noreply.github.com>
2022-09-18 19:51:11 +0300

    separated the function to make it more readable
    carmi was too lazy to write it so god did

--- src/main/java/frc/trigon/robot/utilities/ShootingCalculations.java
+++ src/main/java/frc/trigon/robot/utilities/ShootingCalculations.java
@@ -7,32 +7,22 @@ public class ShootingCalculations {
     static ArrayList<Waypoint> waypoints = new ArrayList<>();
 
     public static double getShootingVelocityFromDistance(double distance) {
-        for(int i = 0; i < distance; i++) {
-            if(waypoints.get(i).distance == distance) {
-                return waypoints.get(i).velocity;
-            }
-
-            if(waypoints.get(i).distance < distance && waypoints.get(i + 1).distance > distance) {
-                Translation2d firstPoint = new Translation2d(waypoints.get(i).distance, waypoints.get(i).velocity);
-                Translation2d secondPoint = new Translation2d(
-                        waypoints.get(i + 1).distance, waypoints.get(i + 1).velocity);
-                return calculateBetweenTranslations(firstPoint, secondPoint, distance);
-            }
+        for(int i = 0; i < waypoints.size(); i++) {
+           if(isPointSmaller(distance, i)){
+               Translation2d firsPoint = new Translation2d(waypoints.get(i).distance, waypoints.get(i).velocity);
+               Translation2d secondPoint = new Translation2d(waypoints.get(i-1).distance, waypoints.get(i-1).velocity);
+               return calculateBetweenTranslations(firsPoint, secondPoint, distance);
+           }
         }
         return 0;
     }
 
     public static double getShootingAngelFromDistance(double distance) {
         for(int i = 0; i < distance; i++) {
-            if(waypoints.get(i).distance == distance) {
-                return waypoints.get(i).angle;
-            }
-
-            if(waypoints.get(i).distance < distance && waypoints.get(i + 1).distance > distance) {
-                Translation2d firstPoint = new Translation2d(waypoints.get(i).distance, waypoints.get(i).angle);
-                Translation2d secondPoint = new Translation2d(
-                        waypoints.get(i + 1).distance, waypoints.get(i + 1).angle);
-                return calculateBetweenTranslations(firstPoint, secondPoint, distance);
+            if(isPointSmaller(distance, i)){
+                Translation2d firsPoint = new Translation2d(waypoints.get(i).distance, waypoints.get(i).angle);
+                Translation2d secondPoint = new Translation2d(waypoints.get(i-1).distance, waypoints.get(i-1).angle);
+                return calculateBetweenTranslations(firsPoint, secondPoint, distance);
             }
         }
         return 0;
@@ -42,6 +32,10 @@ public class ShootingCalculations {
             Translation2d firstPoint, Translation2d secondPoint, double pointToKnow) {
         double m = (firstPoint.getY() - secondPoint.getY()) / (firstPoint.getX() - secondPoint.getX());
         return m * pointToKnow - m * firstPoint.getX() + firstPoint.getY();
+    }
+
+    public static boolean isPointSmaller(double point, int i){
+        return point < waypoints.get(i).distance;
     }
 
     public static void addWaypoint(double distance, double velocity, double angle) {
