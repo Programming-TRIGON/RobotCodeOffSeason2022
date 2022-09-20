93484e6b207831bda8862918616923c8e4e403a4
shay <shaymoshe2008@gmail.com>
2022-09-21 00:38:31 +0300

    pid functin

--- src/main/java/frc/trigon/robot/subsystem/pitcher/PitcherConstants.java
+++ src/main/java/frc/trigon/robot/subsystem/pitcher/PitcherConstants.java
@@ -1,15 +1,28 @@
 package frc.trigon.robot.subsystem.pitcher;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.sensors.CANCoder;
+
+import java.util.Set;
 
 public class PitcherConstants {
-    static final int MIN_TICKS = 300, MUX_TICKS =4000;
+    static final int MIN_TICKS = 0, MAX_TICKS =4000,TICKS_OFFSET = 0;
     static final double GEAR_RATIO = 10;
     private static final int MOTOR_ID = 0;
     final static WPI_TalonSRX  MOTOR = new WPI_TalonSRX(MOTOR_ID);
-    static final double P = 0.01, I = 0, D = 0;
-
+   private static final double P = 0.01, I = 0, D = 0;
+    private static final boolean INVERTED = false;
     static {
-        MOTOR.config_kP();
+        MOTOR.config_kP(0,P);
+        MOTOR.config_kI(0,I);
+        MOTOR.config_kD(0,D);
+
+        MOTOR.setInverted(INVERTED);
+
+        MOTOR.configForwardSoftLimitEnable(true);
+        MOTOR.configForwardSoftLimitThreshold(MAX_TICKS);
+        MOTOR.configReverseSoftLimitEnable(true);
+        MOTOR.configReverseSoftLimitThreshold(MIN_TICKS);
+        
     }
 }
