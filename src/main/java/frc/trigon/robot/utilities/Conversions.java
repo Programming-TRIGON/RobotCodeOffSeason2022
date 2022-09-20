3e6f3f6cc3a028723b93d582a9694c7b016d1cf5
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-09-20 16:36:40 +0300

    editing the documentation in the Conversions document

--- src/main/java/frc/trigon/robot/utilities/Conversions.java
+++ src/main/java/frc/trigon/robot/utilities/Conversions.java
@@ -8,6 +8,7 @@ public class Conversions {
      * Talon FX/SRX and Victor SPX can be configured to adjust their outputs in response to the battery
      * voltage measurement (in all control modes). Use the voltage compensation saturation config to
      * determine what voltage represents 100% output.
+     * The compensated power is the voltage divided by the saturation of the compensation
      *
      * @param voltage the voltage
      * @param voltageCompensationSaturation the saturation of the compensation
