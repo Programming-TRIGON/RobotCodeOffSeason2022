ffd0e166fce107507fe197b640bd7d2e8bea7521
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-09-28 19:03:47 +0300

    documentation

--- src/main/java/frc/trigon/robot/utilities/Conversions.java
+++ src/main/java/frc/trigon/robot/utilities/Conversions.java
@@ -1,7 +1,20 @@
 package frc.trigon.robot.utilities;
 
 public class Conversions {
-    public static double voltageToCompensatedPower(double voltage, double voltageCompensationSaturation) {
+
+        /**
+        * Converts voltage to compensated power
+        *
+        * The voltage compensation saturation will determine what voltage represents 100% output.
+        *
+        * The compensated power is the power resulting from turning the voltage off and on without stopping.
+        * It's basically the voltage divided by the saturation of the compensation.
+        *
+        * @param voltage the voltage
+        * @param voltageCompensationSaturation the saturation of the compensation
+        * @return the compensated power
+        */
+        public static double voltageToCompensatedPower(double voltage, double voltageCompensationSaturation) {
         return voltage/voltageCompensationSaturation;
     }
 }
