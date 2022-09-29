939735a3d1a9357e924d733d48dcf0e2fb175400
ArielSegal1 <segal.ariel.il@gmail.com>
2022-09-29 13:55:10 +0300

    added a command to run thte motors  and added documetation.

--- src/main/java/frc/trigon/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/trigon/robot/subsystems/shooter/Shooter.java
@@ -4,8 +4,12 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.utilities.Conversions;
+
+import java.util.function.DoubleSupplier;
 
 public class Shooter extends SubsystemBase {
     private final static Shooter INSTANCE = new Shooter();
@@ -20,6 +24,10 @@ public class Shooter extends SubsystemBase {
         return INSTANCE;
     }
 
+    /**
+     *
+     * @return the targeted velocity if the motor control mode is velocity, else return 0
+     */
     private double getTargetVelocity() {
         if(!masterMotor.getControlMode().equals(ControlMode.Velocity)) {
             return 0;
@@ -27,8 +35,20 @@ public class Shooter extends SubsystemBase {
         return masterMotor.getClosedLoopTarget();
     }
 
+    /**
+     *
+     * @param velocity the velocity the motors need to get to
+     */
     private void setTargetVelocity(double velocity) {
         masterMotor.set(ControlMode.Velocity, velocity, DemandType.ArbitraryFeedForward, ShooterConstants.S);
+    }
+
+    /**
+     * stop the motors
+     */
+    private void stopMotor()
+    {
+        masterMotor.stopMotor();
     }
 
     /**
@@ -52,13 +72,23 @@ public class Shooter extends SubsystemBase {
         ballCount = 0;
     }
 
-    private boolean atTarget() {
-        return getTargetVelocity() == getCurrentVelocity();
+    /**
+     *
+     * @return the current error from the target velocity
+     */
+    private double getCurrentError() {
+        return getTargetVelocity() - getCurrentVelocity();
     }
 
-    private double currentError()
-    {
-        return getTargetVelocity()-getCurrentVelocity();
+    /**
+     *
+     * @param desiredVelocity the velocity the motors need to get to.
+     * @return an execute command that calls the setTargetVelocity function to set the motors to the targeted velocity
+     */
+    public Command getPrimeShooterCommand(DoubleSupplier desiredVelocity) {
+        return new RunCommand(() -> setTargetVelocity(desiredVelocity.getAsDouble()), this)
+                .andThen(this::stopMotor);
     }
+
 }
 
