875f351962b7cc625a519950399769b7f7e8facc
shay <shaymoshe2008@gmail.com>
2022-09-29 13:20:52 +0300

    gearRatio

--- src/main/java/frc/trigon/robot/subsystem/pitcher/Pitcher.java
+++ src/main/java/frc/trigon/robot/subsystem/pitcher/Pitcher.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Pitcher extends SubsystemBase {
     private final static Pitcher INSTANCE = new Pitcher();
     private final WPI_TalonSRX motor = PitcherConstants.MOTOR;
-    
+
     public static Pitcher getInstance() {
         return INSTANCE;
     }
@@ -17,18 +17,6 @@ public class Pitcher extends SubsystemBase {
 
     public double getDegrees() {
         return 0;
-    }
-
-    public double ticksToDegrees(double ticks) {
-        double rotations = ticks / 4096;
-        double degrees = rotations * 360;
-        return degrees / PitcherConstants.GEAR_RATIO;
-    }
-
-    public double degreesToTicks(double degrees) {
-        double rotations = degrees / 360;
-        double ticks = rotations * 4096;
-        return ticks / PitcherConstants.GEAR_RATIO;
     }
 }
 
--- src/main/java/frc/trigon/robot/utilities/Conversions.java
+++ src/main/java/frc/trigon/robot/utilities/Conversions.java
@@ -2,4 +2,16 @@ package frc.trigon.robot.utilities;
 
 public class Conversions {
     public static final int MAG_TICKS = 4096;
+
+    public double ticksToDegrees(double ticks, double gearRatio) {
+        double rotations = ticks / 4096;
+        double degrees = rotations * 360;
+        return degrees / gearRatio;
+    }
+
+    public double degreesToTicks(double degrees, double gearRatio) {
+        double rotations = degrees / 360;
+        double ticks = rotations * 4096;
+        return ticks / gearRatio;
+    }
 }
