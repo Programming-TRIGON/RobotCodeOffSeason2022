aece7547a5f4d9136722d31e69bee3256c2600ad
idoc07 <108226408+idoc07@users.noreply.github.com>
2022-10-01 23:33:49 +0300

    making VOLTAGE_COMPENSATION_SATURATION default. switching the places of the lines in LoaderConstants

--- src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/loader/LoaderConstants.java
@@ -4,13 +4,13 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.trigon.robot.utilities.Conversions;
 
 public class LoaderConstants {
+    private static final int MOTOR_ID = 0;
+    static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(MOTOR_ID);
+    static final double VOLTAGE_COMPENSATION_SATURATION = 10.5;
     private static final double
             LOAD_VOLTAGE = 6,
             EJECT_VOLTAGE = -6;
-    public static final double VOLTAGE_COMPENSATION_SATURATION = 10.5;
     private static final boolean MOTOR_INVERTED = false;
-    private static final int MOTOR_ID = 0;
-    static final WPI_TalonSRX MOTOR = new WPI_TalonSRX(MOTOR_ID);
 
     static {
         MOTOR.setInverted(MOTOR_INVERTED);
