6b5dbfe1e6ca16c41f6ab14dafb8851d7b4eaa7c
Mboesn <yishayamir@gmail.com>
2022-10-02 17:32:29 +0300

    `getPitcherCommand()` and `stop()`

--- src/main/java/frc/trigon/robot/subsystem/pitcher/Pitcher.java
+++ src/main/java/frc/trigon/robot/subsystem/pitcher/Pitcher.java
@@ -1,8 +1,12 @@
 package frc.trigon.robot.subsystem.pitcher;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.utilities.Conversions;
+
+import java.util.function.DoubleSupplier;
 
 public class Pitcher extends SubsystemBase {
     private final static Pitcher INSTANCE = new Pitcher();
@@ -26,6 +30,19 @@ public class Pitcher extends SubsystemBase {
         double offsettedRead = Conversions.offsetRead(motor.getSelectedSensorPosition(), PitcherConstants.MIN_TICKS);
         double motorAngle = Conversions.magTicksToDegrees(offsettedRead);
         return Conversions.motorPositionToSystemPosition(motorAngle, PitcherConstants.GEAR_RATIO);
+    }
+
+    public void stop() {
+        motor.disable();
+    }
+
+    /**
+     * @param targetAngle the command will continuously set the target angle to this
+     * @return a command that sets the angle of the pitcher according to the given supplier.
+     */
+    public Command getPitchingCommand(DoubleSupplier targetAngle) {
+        return new RunCommand(() -> setTargetAngle(targetAngle.getAsDouble()), this)
+                .andThen(this::stop, this);
     }
 }
 
