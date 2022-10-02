012c8b778f592538f226d009c99e928289a25241
ArielSegal1 <segal.ariel.il@gmail.com>
2022-10-02 18:20:29 +0300

    refactoring

--- src/main/java/frc/trigon/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/trigon/robot/subsystems/shooter/Shooter.java
@@ -3,7 +3,6 @@ package frc.trigon.robot.subsystems.shooter;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
