f909eeeae14f4c0a4f8f9f54926d441ee0d3c243
thesnipe12 <yishi178@gmail.com>
2022-10-03 19:05:13 +0300

    Changed the method all to getWaypointFromDistance, how it works and deleted isPointSmaller

--- src/main/java/frc/trigon/robot/utilities/ShootingCalculations.java
+++ src/main/java/frc/trigon/robot/utilities/ShootingCalculations.java
@@ -11,24 +11,25 @@ import java.util.Objects;
 public class ShootingCalculations {
     private static final ArrayList<Waypoint> waypoints = new ArrayList<>();
 
-    public static Waypoint all(double distance) {
+    /**
+     * @param distance the distance from the target
+     * @return a waypoint with the distance, velocity and angle from the given distance
+     */
+    public static Waypoint getWaypointFromDistance(double distance) {
         Waypoint[] waypointsFromDistance = getWaypointsFromDistance(distance);
         if(waypointsFromDistance == null) {
             return null;
         }
-        double velocity = calculateM(
-                waypointsFromDistance[0].distance,
-                waypointsFromDistance[0].velocity,
-                waypointsFromDistance[1].distance,
-                waypointsFromDistance[1].velocity,
-                distance);
-        double angle = calculateM(
-                waypointsFromDistance[0].distance,
-                waypointsFromDistance[0].angle,
-                waypointsFromDistance[1].distance,
-                waypointsFromDistance[1].angle,
-                distance);
-
+        double velocity = calculateBetweenTranslations(
+                new Translation2d(waypointsFromDistance[0].distance, waypointsFromDistance[0].velocity),
+                new Translation2d(waypointsFromDistance[1].distance, waypointsFromDistance[1].velocity),
+                distance
+        );
+        double angle = calculateBetweenTranslations(
+                new Translation2d(waypointsFromDistance[0].distance, waypointsFromDistance[0].angle),
+                new Translation2d(waypointsFromDistance[1].distance, waypointsFromDistance[1].angle),
+                distance
+        );
         return new Waypoint(distance, velocity, angle);
     }
 
@@ -39,7 +40,7 @@ public class ShootingCalculations {
      * @return the velocity needed for the shooter
      */
     public static double getShootingVelocityFromDistance(double distance) {
-        return Objects.requireNonNullElse(all(distance), new Waypoint(0, 0, 0)).velocity;
+        return Objects.requireNonNullElse(getWaypointFromDistance(distance), new Waypoint(0, 0, 0)).velocity;
     }
 
     /**
@@ -49,8 +50,7 @@ public class ShootingCalculations {
      * @return the angle needed for the shooter
      */
     public static double getShootingAngleFromDistance(double distance) {
-
-        return Objects.requireNonNullElse(all(distance), new Waypoint(0, 0, 0)).angle;
+        return Objects.requireNonNullElse(getWaypointFromDistance(distance), new Waypoint(0, 0, 0)).angle;
     }
 
     /**
@@ -68,17 +68,6 @@ public class ShootingCalculations {
     }
 
     /**
-     * Checks if the given point is smaller than the point in the array point in the array.
-     *
-     * @param point the point to check
-     * @param index the index of a point in the array
-     * @return whether the given point is smaller than the point in the array
-     */
-    public static boolean isPointSmaller(double point, int index) {
-        return point < waypoints.get(index).distance;
-    }
-
-    /**
      * Adds a waypoint to the waypoints list.
      *
      * @param distance the distance for the waypoint
@@ -93,20 +82,13 @@ public class ShootingCalculations {
     private static Waypoint[] getWaypointsFromDistance(double distance) {
         Waypoint[] toReturn = new Waypoint[2];
         for(int i = 0; i < waypoints.size(); i++) {
-            if(isPointSmaller(distance, i)) {
+            if(distance < waypoints.get(i).distance) {
                 toReturn[0] = waypoints.get(i);
                 toReturn[1] = waypoints.get(i - 1);
                 return toReturn;
             }
         }
         return null;
-    }
-
-    public static double calculateM(double x1, double y1, double x2, double y2, double distance) {
-        Translation2d
-                firstPoint = new Translation2d(x1, y1),
-                secondPoint = new Translation2d(x2, y2);
-        return calculateBetweenTranslations(firstPoint, secondPoint, distance);
     }
 
     private static class Waypoint {
