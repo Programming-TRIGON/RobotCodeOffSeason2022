a01f8fa2c57f4da697b26e110a396ffe0584c561
thesnipe12 <yishi178@gmail.com>
2022-10-03 13:07:27 +0300

    Fixed some javadoc, added the getEjectCommand and getLoadCommand methods and made getState look better

--- src/main/java/frc/trigon/robot/subsystems/loader/Loader.java
+++ src/main/java/frc/trigon/robot/subsystems/loader/Loader.java
@@ -1,6 +1,8 @@
 package frc.trigon.robot.subsystems.loader;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.trigon.robot.utilities.Conversions;
 
@@ -28,12 +30,35 @@ public class Loader extends SubsystemBase {
     /**
      * Sets the state of the loader.
      *
-     * @param state the wanted state for the loader out of the three possible states: LOAD, EJECT and OFF.
+     * @param state the wanted state of the loader as a LoaderState
      */
-
     public void setState(LoaderState state) {
-        motor.set(Conversions.voltageToCompensatedPower(state.voltage, LoaderConstants.VOLTAGE_COMPENSATION_SATURATION));
+        double compensatedPower = Conversions.voltageToCompensatedPower(
+                state.voltage, LoaderConstants.VOLTAGE_COMPENSATION_SATURATION);
+        motor.set(compensatedPower);
         currentState = state;
+    }
+
+    /**
+     * @return a command that makes the loader eject when it starts, and turns it off when it ends.
+     */
+    public Command getEjectCommand() {
+        return new StartEndCommand(
+                () -> setState(LoaderState.EJECT),
+                () -> setState(LoaderState.OFF),
+                this
+        );
+    }
+
+    /**
+     * @return a command that makes the transporter load when it starts, and turns it off when it ends.
+     */
+    public Command getLoadCommand() {
+        return new StartEndCommand(
+                () -> setState(LoaderState.LOAD),
+                () -> setState(LoaderState.OFF),
+                this
+        );
     }
 }
 
--- src/main/java/frc/trigon/robot/utilities/Conversions.java
+++ src/main/java/frc/trigon/robot/utilities/Conversions.java
@@ -2,20 +2,20 @@ package frc.trigon.robot.utilities;
 
 public class Conversions {
 
-        /**
-        * Converts voltage to compensated power
-        *
-        * The voltage compensation saturation will determine what voltage represents 100% output.
-        *
-        * The compensated power is the power resulting from turning the voltage off and on without stopping.
-        * In order to find the compensated power we have to divide the voltage by the voltage compensation
-        * saturation.
-        *
-        * @param voltage the voltage of the loader
-        * @param voltageCompensationSaturation the saturation of the compensation
-        * @return the compensated power resulting from turning the voltage off and on without stopping
-        */
-        public static double voltageToCompensatedPower(double voltage, double voltageCompensationSaturation) {
-            return voltage/voltageCompensationSaturation;
+    /**
+     * Converts voltage to compensated power.
+     * <p>
+     * The voltage compensation saturation will determine what voltage represents 100% output.
+     * <p>
+     * The compensated power is the power resulting from turning the voltage off and on without stopping.
+     * In order to find the compensated power we have to divide the voltage by the voltage compensation
+     * saturation.
+     *
+     * @param voltage                       the voltage of the loader
+     * @param voltageCompensationSaturation the saturation of the compensation
+     * @return the compensated power resulting from turning the voltage off and on without stopping
+     */
+    public static double voltageToCompensatedPower(double voltage, double voltageCompensationSaturation) {
+        return voltage / voltageCompensationSaturation;
     }
 }
