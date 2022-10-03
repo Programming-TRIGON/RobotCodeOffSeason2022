d2aa9a4c6ef708210d1ce2d352ba07914715991e
gabriel <gsymodiin@gmail.com>
2022-10-03 23:50:55 +0300

    deleted newline

--- src/main/java/frc/trigon/robot/subsystems/swerve/Swerve.java
+++ src/main/java/frc/trigon/robot/subsystems/swerve/Swerve.java
@@ -8,7 +8,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Swerve extends SubsystemBase {
-
     private final static Swerve INSTANCE = new Swerve();
 
     private Swerve() {
