dbd0fd24878decdbc5b016002aca1c87f5110afb
thesnipe12 <yishi178@gmail.com>
2022-10-06 18:26:59 +0300

    Changed the voltageToCompensatedPower's javadoc

--- src/main/java/frc/trigon/robot/utilities/Conversions.java
+++ src/main/java/frc/trigon/robot/utilities/Conversions.java
@@ -10,6 +10,7 @@ public class Conversions {
      * The compensated power is the power resulting from turning the voltage off and on without stopping.
      * In order to find the compensated power we have to divide the voltage by the voltage compensation
      * saturation.
+     * //TODO: clear this up
      *
      * @param voltage                       the voltage of the loader
      * @param voltageCompensationSaturation the saturation of the compensation
