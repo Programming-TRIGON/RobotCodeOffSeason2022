3c04a4e3bc49c17cd02335cf9767802f827a4735
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2022-10-11 14:00:40 +0300

    Fixed CloseShooterCommand

--- src/main/java/frc/trigon/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/shooter/ShooterConstants.java
@@ -6,7 +6,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 public class ShooterConstants {
     public static final double
             IDLE_TARGET_VELOCITY = 2500,
-            CLOSE_SHOOTING_TARGET_VELOCITY = 1000;
+            CLOSE_SHOOTING_TARGET_VELOCITY = 1500;
     private static final int
             LEFT_MOTOR_ID = 12,
             RIGHT_MOTOR_ID = 6;
