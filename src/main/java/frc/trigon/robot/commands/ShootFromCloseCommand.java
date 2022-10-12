72c6e3fa419c744dea7c6f9795eebe2330a38e32
thesnipe12 <yishi178@gmail.com>
2022-10-12 09:20:05 +0300

    Added velocity to dashboard

--- src/main/java/frc/trigon/robot/commands/ShootFromCloseCommand.java
+++ src/main/java/frc/trigon/robot/commands/ShootFromCloseCommand.java
@@ -1,12 +1,12 @@
 package frc.trigon.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import frc.trigon.robot.subsystems.loader.Loader;
 import frc.trigon.robot.subsystems.pitcher.Pitcher;
 import frc.trigon.robot.subsystems.pitcher.PitcherConstants;
 import frc.trigon.robot.subsystems.shooter.Shooter;
-import frc.trigon.robot.subsystems.shooter.ShooterConstants;
 import frc.trigon.robot.subsystems.transporter.Transporter;
 
 public class ShootFromCloseCommand extends ParallelCommandGroup {
@@ -15,8 +15,11 @@ public class ShootFromCloseCommand extends ParallelCommandGroup {
     public ShootFromCloseCommand() {
         super();
         canShootBtn = new Button(() -> canShoot() && isScheduled());
+        if(SmartDashboard.getNumber("SHOOT_FROM_CLOSE_VELOCITY", -400) == -400)
+            SmartDashboard.putNumber("SHOOT_FROM_CLOSE_VELOCITY", 0);
         addCommands(
-                Shooter.getInstance().getPrimeShooterCommand(() -> ShooterConstants.CLOSE_SHOOTING_TARGET_VELOCITY),
+                Shooter.getInstance()
+                        .getPrimeShooterCommand(() -> SmartDashboard.getNumber("SHOOT_FROM_CLOSE_VELOCITY", 0)),
                 Pitcher.getInstance().getPitchingCommand(() -> PitcherConstants.CLOSE_SHOOTING_TARGET_ANGLE)
         );
         canShootBtn.whileHeld(
